ae599e21bb44d790ab9907e169420a9bc5a4b3bf
iamiceice <zgslzgsl@gmail.com>
2023-11-22 18:26:06 +0800

    homework5

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use crate::homework41::hw41;
 use crate::homework42::hw42;
 use crate::homework43::hw43;
 
+macro_rules! minus {
+    ($a:expr,$b:expr) => {$a-$b};
+}
 mod garden;
 mod homework41;
 mod homework42;
@@ -12,4 +15,6 @@ fn main() {
     hw41();
     hw42();
     hw43();
+    let x=minus!(3,2);
+    println!("{x}");
 }
